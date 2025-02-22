33a575f65ca6c31636f5c00c434dddb595837030
Kheyssper <gaspar.cossa@uzambeze.ac.mz>
2025-02-22 02:41:30 +0200

    Closing the meme

--- Main.jsx
+++ Main.jsx
@@ -1,65 +1,49 @@
-import React from "react"
-import Ingredients from "./Ingredients"
-import Recipe from "./Recipe"
-import { getRecipeFromMistral } from "./ai"
-
-/**
- * Challenge: Get a recipe from the AI!
- * 
- * This will be a bit harder of a challenge that will require you
- * to think critically and synthesize the skills you've been
- * learning and practicing up to this point.
- * 
- * Using either the `getRecipeFromChefClaude` function or the 
- * `getRecipeFromMistral` function, make it so that when the user
- * clicks "Get a recipe", the text response from the AI is displayed
- * in the <ClaudeRecipe> component.
- * 
- * For now, just have it render the raw markdown that the AI returns,
- * don't worry about making it look nice yet. (We're going to use a
- * package that will render the markdown for us soon.)
- */
+import { useState } from "react"
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState(
-        []
-    )
-    const [aiRecipe, setAiRecipe] = React.useState('');
-
-
-    async function getRecipe() {
-            const recipe = await getRecipeFromMistral(ingredients);
-            setAiRecipe(recipe);
-    }
-
-    function addIngredient(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const newIngredient = formData.get("ingredient");
-        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
-        event.target.reset();
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
 
     return (
         <main>
-            <form onSubmit={addIngredient} className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button type="submit">Add ingredient</button>
-            </form>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
 
-            {ingredients.length > 0 &&
-                <Ingredients
-                    ingredients={ingredients}
-                    handleRecipe={getRecipe}
-                />
-            }
-
-            {aiRecipe && <Recipe aRecipe={aiRecipe} />}
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
         </main>
     )
 }
